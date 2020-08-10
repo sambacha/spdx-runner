2071cfd1a52e7ab25a4342825e1efe944ccdc4ec
sam <sam@freighttrust.com>
2020-08-10 02:01:39 -0700

    fixing testing
    Signed-off-by: sam <sam@freighttrust.com>

--- package/tests/index.js
+++ package/tests/index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json("Hello world");
+    res.status(200).json("Hello world");
 });
 
 module.exports = app.listen(4000, () =>
-  console.log(`Running on http://localhost:4000`)
+    console.log("Running on http://localhost:4000")
 );
