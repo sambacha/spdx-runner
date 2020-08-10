03092cb954bf313d2a0872b22691d2773ed4c44b
sam <sam@freighttrust.com>
2020-08-10 00:40:27 -0700

    feat: release 1.0.0
    rewrote scripts, added some new functionality, and unit testing. Scripts now inventory, alert, and insert missing SPDX license headers README now contains documentation on what scripts do what, etc.
    
    Examples folder contains two diff. examples: one is just vanilla testing (the javascript files) the other contains Solidity files, which have a more peculiar implementation so far for including SPDX, I am unsure if SPDX on certain `pragma` definitions is best practices or not.

--- dist.sh
+++ dist.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# GPG Fingerprint 
 if [ -z $NOSIGN ]; then
   echo "==> Signing..."
-  shasum -a256 * > ./freight_${VERSION}_SHA256SUMS
-  gpg --default-key 6F6EB43E --detach-sig ./freight_${VERSION}_SHA256SUMS
+  shasum -a256 * > ./spdx${VERSION}_SHA256SUMS
+  gpg --default-key 6F6EB43E --detach-sig ./spdx${VERSION}_SHA256SUMS
 fi
---
+++ spdx-header.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+$ORG=
+cat << EOF > license_header
+/*SPDX-License-Identifier: Apache-2.0
+SPDXVersion: SPDX-2.2
+SPDX-FileCopyrightText: Copyright 2020 $ORG */
+EOF
