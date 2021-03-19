f19f24f076dfd91950b3ca7da2d92a8c4eaad7fe
sam <sam@freighttrust.com>
2021-03-18 20:54:16 -0700

    refactor(python): init
    Signed-off-by: sam <sam@freighttrust.com>

--- dist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# GPG Fingerprint 
-if [ -z $NOSIGN ]; then
-  echo "==> Signing..."
-  shasum -a256 * > ./spdx${VERSION}_SHA256SUMS
-  gpg --default-key 6F6EB43E --detach-sig ./spdx${VERSION}_SHA256SUMS
-fi
